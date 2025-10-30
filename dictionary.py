75b4953c798e3ff35b57c290a60611c3bf0ade3a
doancp <doanpchristopher@gmail.com>
2025-10-30 02:51:57 -0400

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,35 @@
+import requests
+import tkinter as tk
+
+def dict_entry():
+    
+    dict_term = input("Enter Word:").lower()
+    url = f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{dict_term}?key=8xxxxxxxxxxxxxxx" # insert your dictionary key here 
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+            
+        if isinstance(data, list) and len(data) > 0 and isinstance(data[0], dict):
+            for entry in data:
+                print(entry["meta"]["id"])
+
+
+
+root = tk.Tk()
+root.title("Dictionary")
+root.geometry("500x500")
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
+
+
