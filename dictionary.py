4b6bc2a829b41942d1b2c13839ec29d991270be2
doancp <doanpchristopher@gmail.com>
2025-11-05 01:57:25 -0500

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -3,17 +3,25 @@ import tkinter as tk
 
 def dict_entry():
     
-    dict_term = input("Enter Word:").lower()
-    url = f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{dict_term}?key=xxxxxxxxxxxxxxx" # insert your dictionary key here 
+    dict_term = entry.get().lower()
+    url = f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{dict_term}?key=XXXXXXXXXXXXXXXXXXXXXXXXX" # insert your key here
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
-            
-        if isinstance(data, list) and len(data) > 0 and isinstance(data[0], dict):
-            for entry in data:
-                print(entry["meta"]["id"])
+        
+        dict_def.config(text=f"Term: {data[0]['meta']['id']}")
 
+
+        if isinstance(data, list) and len(data) > 0 and isinstance(data[0], dict):
+            results = [] 
+            for term in data:
+                results.append(term["meta"]["id"])
+                for meaning in term["shortdef"]:
+                    results.append(" -" + meaning) 
+            
+            result_output = "\n".join(results)
+            result_label.config(text=result_output)
 
 
 root = tk.Tk()
@@ -21,6 +29,17 @@ root.title("Dictionary")
 root.geometry("500x500")
 
 
+entry = tk.Entry(root)
+entry.pack(pady=30)
+
+dict_button = tk.Button(root, text="Submit Word", command=dict_entry)
+dict_button.pack(pady=30)
+
+dict_def = tk.Label(root, text="")
+dict_def.pack(pady=30)
+
+result_label = tk.Label(root, text="")
+result_label.pack()
 
 
 
@@ -28,8 +47,7 @@ root.geometry("500x500")
 
 
 
-
+button = tk.Button(root, text="Quit", font=('Arial', 20), fg=("Red"),bg=("black"), command=root.destroy) # quit button
+button.place(x=400, y=400)
 
 root.mainloop()
-
-
